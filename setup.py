fa5d5d4e966f4e966c8dd2d3d650409462f1d7b2
eruizgar91 <enrique@oceanprotocol.com>
2018-11-12 09:40:00 +0100

    Bumpversion for new release.

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/osmosis-driver-interface',
-    version='0.0.2',
+    version='0.0.3',
     zip_safe=False,
 )
