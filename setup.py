7be00246a41d60df2b80969656326f2ba5672b80
ssallam <travis@travis-ci.org>
2020-09-16 09:58:22 +0200

    Bump version: 0.0.7 → 0.1.0

--- setup.py
+++ setup.py
@@ -71,6 +71,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/osmosis-driver-interface',
-    version='0.0.7',
+    version='0.1.0',
     zip_safe=False,
 )
